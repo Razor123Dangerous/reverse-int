baaafbd2de7ee0b03e0838876000a30899054d16
Razor123Dangerous <romshik1998@gmail.com>
2020-10-25 21:54:07 +0300

    start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let str = Math.abs(n).toString();
+  let str_reversed = '';
+  for(let i = str.length - 1; i >= 0; i--){
+    str_reversed += str[i];
+  }
+  return +str_reversed;
 }
+
